c7508e24e4aff8846e9896da74b9458255ba9234
HONGYEOL CHOI <chykrkr@gmail.com>
2020-03-14 17:20:39 +0900

    Implemented working version

--- App.js
+++ App.js
@@ -1,33 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weahter from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <View style={styles.redView}/>
-      <View style={styles.yellowView}/>
-    </View>
-  );
+const API_KEY = "3023498e0faf419d4962da5c5aac8cdb";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        console.log(error);
+        this.setState({error: error.message})
+      }
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      })
+    });
+  }
+
+  render() {
+    const { isLoaded, error, temperature, name } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true}></StatusBar>
+        {isLoaded ? <Weahter weatherName={name} temp={Math.floor(temperature - 273.15)}/> : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent:'center',
-    alignItems:'center'
   },
-  
-  redView: {
-    height:50,
-    width:50,
-    backgroundColor: 'red'
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
-
-  yellowView: {
-    height:50,
-    width:50,
-    backgroundColor: 'yellow'
+  loading:{
+    flex: 1,
+    backgroundColor:'#FDF6AA',
+    justifyContent: "flex-end",
+    paddingLeft: 24
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
   }
 });
